18726e059076e4108e6b9f99d722ce8c3f40163b
nooleee <NOOL@DESKTOP-RADNT0G>
2024-03-28 18:17:10 +0900

    add User : 생성자 추가

--- consoleBoard/src/consoleBoard/User.java
+++ consoleBoard/src/consoleBoard/User.java
@@ -6,5 +6,12 @@ public class User {
 	private String id, password;
 	private ArrayList<Board> board;
 	
+	public User(String id, String password) {
+		this.id = id;
+		this.password = password;
+		board = new ArrayList<>();
+	}
+	
 	// CRUD
+	
 }
