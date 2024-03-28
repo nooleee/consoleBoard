40a9b359d5b2dce68c9a65285b7516240edc030d
nooleee <NOOL@DESKTOP-RADNT0G>
2024-03-28 18:31:44 +0900

    add BoardManager : 멤버 변수 추가

--- consoleBoard/src/consoleBoard/BoardManager.java
+++ consoleBoard/src/consoleBoard/BoardManager.java
@@ -1,5 +1,10 @@
 package consoleBoard;
 
-public class BoardManager {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+public class BoardManager {
+	private ArrayList<Board> list;
+	private Map<Board, String> boardData = new HashMap<>();
 }
