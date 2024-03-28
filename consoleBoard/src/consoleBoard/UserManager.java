dbcf40b524a57d479eaa7b9a8d48eecf5dc538ec
nooleee <NOOL@DESKTOP-RADNT0G>
2024-03-28 17:58:16 +0900

    add UserManager : 멤버 변수 추가

--- consoleBoard/src/consoleBoard/UserManager.java
+++ consoleBoard/src/consoleBoard/UserManager.java
@@ -1,5 +1,12 @@
 package consoleBoard;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserManager {
+	private ArrayList<User> list;
+	Map<User, ArrayList<Board>> userData = new HashMap<>();
+	
 
 }
