081f587de9ea228879abed60ef5de5c0e1ff819e
nooleee <NOOL@DESKTOP-RADNT0G>
2024-03-29 13:51:59 +0900

    add Board : 멤버 변수 추가 및 생성자 수정

--- consoleBoard/src/consoleBoard/Board.java
+++ consoleBoard/src/consoleBoard/Board.java
@@ -2,10 +2,16 @@ package consoleBoard;
 
 public class Board {
 	private String title, contents;
+	private int code;
 	
-	public Board(String title, String contents) {
+	public Board() {
+		
+	}
+	
+	public Board(String title, String contents, int code) {
 		this.title = title;
 		this.contents = contents;
+		this.code = code;
 	}
 	
 	public String getTitle() {
@@ -16,7 +22,16 @@ public class Board {
 		return this.contents;
 	}
 	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	
+	
 	public Board clone() {
-		return new Board(this.title, this.contents);
+		return new Board(this.title, this.contents, this.code);
 	}
 }
