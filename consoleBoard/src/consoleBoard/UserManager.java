dfdd3e057278af5079e150b674e0e0c798d0322f
nooleee <NOOL@DESKTOP-RADNT0G>
2024-03-29 10:23:08 +0900

    add UserManager : findUserByUserId 메소드 추가

--- consoleBoard/src/consoleBoard/UserManager.java
+++ consoleBoard/src/consoleBoard/UserManager.java
@@ -6,7 +6,30 @@ import java.util.Map;
 
 public class UserManager {
 	private ArrayList<User> list;
-	private Map<User, ArrayList<Board>> userData = new HashMap<>();
+	private Map<String, ArrayList<Board>> userData = new HashMap<>();	// userData = (password/list, password/list, ...)
 	
+	public User createUser(String id, String password) {
+		if(!findUserByUserId(id)) {
+			User user = new User(id, password);
+			list.add(user);
+			return user.clone();
+		}
+	}
+	
+	private boolean findUserByUserId(String id) {
+		for(User user : list) {
+			if(user.getId().equals(id))
+				return true;
+		}
+		return false;
+	}
+	
+	public ArrayList<User> findUserAll() {
+		ArrayList<User> copy = new ArrayList<>();
+		for(User user : list) {
+			copy.add(user.clone());
+		}
+		return copy;
+	}
 
 }
