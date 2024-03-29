f89461ef662675e5c398cb3ddc94c0de9998dfa1
nooleee <NOOL@DESKTOP-RADNT0G>
2024-03-29 10:10:11 +0900

    add Board : 생성자 추가

--- consoleBoard/src/consoleBoard/Board.java
+++ consoleBoard/src/consoleBoard/Board.java
@@ -2,4 +2,9 @@ package consoleBoard;
 
 public class Board {
 	private String title, contents;
+	
+	public Board(String title, String contents) {
+		this.title = title;
+		this.contents = contents;
+	}
 }
